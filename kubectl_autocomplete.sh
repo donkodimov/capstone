77db142fc90c049d1fcb1d51a47f391d6a36994b
Donko Dimov <donko@abv.bg>
2020-03-30 16:49:32 +0200

    feat: Create script for kubectl autocompletion

---
+++ kubectl_autocomplete.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source <(kubectl completion bash)
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+echo "alias k=kubectl" >> ~/.bashrc
+echo "complete -F __start_kubectl k" >> ~/.bashrc
